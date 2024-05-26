ad9599e17905bf4b00455d3f8728ab42a938c588
Phillip Matambanadzo <phillip.matambanadzo@kisscoders.ruhr>
2024-05-26 09:46:59 +0100

    chore: fix eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,16 @@
 import js from "@eslint/js";
-import typescriptEslintParser from "@typescript-eslint/parser";
 import eslintConfigPrettier from "eslint-config-prettier";
 import tailwind from "eslint-plugin-tailwindcss";
 import pluginVue from "eslint-plugin-vue";
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt([
-  ...tailwind.configs["flat/recommended"],
+export default withNuxt(
+  tailwind.configs["flat/recommended"],
   eslintConfigPrettier,
-]).prepend(js.configs.recommended, ...pluginVue.configs["flat/recommended"], [
-  {
-    files: ["**/*.vue"],
-    languageOptions: {
-      parser: typescriptEslintParser,
-      sourceType: "module",
-    },
+)
+  .prepend(js.configs.recommended, ...pluginVue.configs["flat/recommended"])
+  .override("tailwindcss:rules", {
     rules: {
-      "vue/no-multiple-template-root": "off",
       "tailwindcss/no-custom-classname": "off",
-      "vue/multi-word-component-names": [
-        "error",
-        {
-          ignores: ["index"],
-        },
-      ],
     },
-  },
-]);
+  });
